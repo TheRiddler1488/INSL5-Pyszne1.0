7093828c8d9107828d74f3b9b0543d36de273525
Pawel-Brzez <brzezicki.pawel@gmail.com>
2023-10-12 17:00:22 +0200

    Dokonczone

--- AliakseiSamultsau/holop_app/models.py
+++ AliakseiSamultsau/holop_app/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model) :
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=150,blank=False,unique=False)
+    year = models.PositiveIntegerField(default=2023)
+
+    def __str__(self):
+        return self.title
